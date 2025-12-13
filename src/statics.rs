44937042c2a110e11a11ded2fe497e13ed8f0c93
smallB007 <atch.cpp@gmail.com>
2025-12-13 09:14:50 +0100

    Cln

--- src/statics.rs
+++ src/statics.rs
@@ -5,8 +5,7 @@ https://docs.rs/console/latest/console/
 */
 use console::style;
 use std::sync::LazyLock;
-use dialoguer::{Select, theme::ColorfulTheme};
-use dialoguer::Input;
+use dialoguer::{Input,Select, theme::ColorfulTheme};
 pub static GREETING: LazyLock<String> = LazyLock::new(|| {
     style("Welcome to bluff!").on_red().bold().to_string()
 });
